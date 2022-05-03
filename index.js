996863f5f7238d4f16983fd06d877dbe2653f994
Thays Omine <thaysomine@gmail.com>
2022-05-03 05:25:30 -0300

    feat: get/post messages added

--- index.js
+++ index.js
@@ -58,8 +58,7 @@ app.post('/participants', async (req, res) => {
         await database.collection('messages').insertOne(loginMessage);
         console.log(loginMessage);
         res.status(201);
-    }
-    catch {
+    } catch {
         res.status(422).send("Erro ao cadastrar");
     }
 });
@@ -70,8 +69,65 @@ app.get('/participants', async (req, res) => {
         console.log(response);
         res.send(response);
     } catch(err) {
-        console.log("deu erro");
-        res.status(500).send("Erro ao buscar");
+        console.log("deu erro get/participans");
+        res.send("Erro ao buscar participantes");
+    }
+});
+
+// post e get para enviar mensagens
+app.post('/messages', async (req, res) => {
+    const {to, text, type} = req.body;
+    console.log('body da mensagem', to, text, type);
+    const user = req.headers.user;
+    const message = {
+        from: user, 
+        to, 
+        text, 
+        type, 
+        time: dayjs().format('HH:mm:ss')
+    };
+    const schema = joi.object({
+        from: joi.string().required(),
+        to: joi.string().required(),
+        text: joi.string().required(),
+        type: joi.string().required(),
+        time: joi.string().required()
+    });
+    const validation = schema.validate(message);
+    const validateType = (type === 'message' || type === 'private_message');
+    if (validation.error || !validateType) {
+        console.log('Erro de validação');
+        res.status(422).send('Erro de validação');
+        return;
+    }
+
+    try {
+        const checkName = await database.collection('participants').findOne({name: user});
+        if (!checkName) {
+            res.status(422).send('Participante não cadastrado');
+            console.log('Participante não cadastrado');
+            return;
+        }
+        await database.collection('messages').insertOne(message);
+        console.log(message);
+        res.status(201);
+    } catch {
+        res.status(422).send("Erro ao enviar mensagem");
+    }
+});
+
+app.get('/messages', async (req, res) => {
+    const user = req.headers.user
+    const limit = parseInt(req.query.limit);
+    console.log(limit);
+
+    try {
+        const showMessages = await database.collection('messages').find({$or:[{to:'Todos'}, {from:user}, {to:user}]}).toArray();
+        console.log(showMessages);
+        limit ? res.send(showMessages.slice(0, limit)) : res.send(showMessages); // TODO: checar verificação de limite
+    } catch(err) {
+        console.log("deu erro get/messages");
+        res.send("Erro ao buscar mensagens" + err);
     }
 });
 
